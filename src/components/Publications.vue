77d1f029af2e96a45dbb311ae66cac7b4a2aa89b
Adam Soltys <asoltys@gmail.com>
2017-03-14 10:41:34 -0700

    create components and routes

---
+++ src/components/Publications.vue
@@ -0,0 +1,31 @@
+<template lang="pug">
+  div
+    CCOGHeader
+    main.container(role='main', property='mainContentOfPage')
+      h1 Canadian Council on Geomatics Website Redesign
+      ul
+        li
+          router-link(to="splash") Development Site Splash Page
+    CCOGFooter
+</template>
+
+<script>
+import CCOGHeader from './CCOGHeader'
+import CCOGFooter from './CCOGFooter'
+
+export default {
+  name: 'Landing',
+  components: {
+    CCOGHeader,
+    CCOGFooter
+  }
+}
+</script>
+
+<style lang="stylus">
+  body
+    background none
+
+  td
+    max-width 200px
+</style>
--- src/main.js
+++ src/main.js
@@ -6,12 +6,22 @@ import App from './App'
 import Landing from './components/Landing'
 import Splash from './components/Splash'
 import Welcome from './components/Welcome'
+import About from './components/About'
+import Issues from './components/Issues'
+import Members from './components/Members'
+import Accord from './components/Accord'
+import Publications from './components/Publications'
 
 Vue.config.productionTip = false
 
 const routes = [
   { path: '/', component: Landing },
   { path: '/welcome', component: Welcome },
+  { path: '/about', component: About },
+  { path: '/issues', component: Issues },
+  { path: '/members', component: Members },
+  { path: '/accord', component: Accord },
+  { path: '/publications', component: Publications },
   { path: '/splash', component: Splash }
 ]
 
