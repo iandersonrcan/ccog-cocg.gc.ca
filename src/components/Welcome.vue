f61834ea5502788cef6cf53663853e1d1f52bede
Adam Soltys <asoltys@gmail.com>
2017-03-17 22:38:11 -0700

    drop in sample content from design mockup

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,7 +1,96 @@
 <template lang="pug">
   div
     CCOGHeader
-    main.container-fluid(role='main', property='mainContentOfPage')
+    .banner.welcome
+      .well
+        h1 Welcome to CCOG
+        p.lead
+          | The Canadian Council on Geomatics is the major federal-provincial-territorial consultative body for geomatics.
+    .container
+      .row
+        .col-md-12
+          h2 Geomatics Accord
+        .col-md-8
+          p
+            | The Canadian Geomatics Accord enables intergovernmental collaboration and cooperation on geospatial data sharing, setting of common geographical data standards, and sharing of costs for core national datasets.
+          p
+            | The previous versions of the Accord (2001-2006 and 2007-2012) both achieved broad support in jurisdictions across Canada, and resulted in collaborative practices. These collaborative efforts resulted in core, nationally standardized datasets and a reduction of duplication between governments. These datasets are used in both private and public sector applications, including for agriculture, natural resource development, transportation, environmental assessment, land claims, boundary discussions, and emergency management. They have also been one of the world&apos;s first examples of open data (http://open.canada.ca/).
+          p
+            | Early benefits of the current Accord are materializing as the private sector and governmental organizations increasingly use open geospatial data, tools and technologies. Such data and tools are used in nearly every sector of the economy, including natural resource development (e.g., forestry, oil and gas, and mining), land claims, parks, elections, infrastructure (e.g., roads and rivers), environment (e.g, forests and biodiversity).
+          p
+            | Sharing of data, tools and technologies permitted under the Accord are underpinning critical operations in many jurisdictions. For example, the sharing of data and tools through the Accord is facilitating dispatch of snow removal vehicles in the Yukon; Department of National Defence planning for managing emergencies; and the conduct of elections at various levels across all jurisdictions.
+        .col-md-4
+          img.img-responsive(src='/static/img/heatmap.jpg')
+          |  
+          em Lorem ipsum dolor sit Amet consectetuer.
+    .well
+      .container
+        .row
+          .col-md-12
+            h2 Key points
+          .col-md-6
+            ul
+              li
+                | The use of geospatial information contributed $20.7 billion of Canada&rsquo;s national Gross Domestic Product and generated approximately 19,000 jobs within Canada&rsquo;s economy in 2013.
+              li
+                | Natural Resources Canada connects producers and users of geospatial information via the GeoConnections program, housed within the Canada Centre for Mapping and Earth Observation.
+          .col-md-6
+            ul
+              li
+                | Geomatics is the modern discipline of gathering, storing, processing, and delivering geographic information.
+              li
+                | Geographic Information System technology, or GIS, was invented in Canada in the 1960s, leading to rapid innovation in the use and application of geospatial data.
+    .container
+      .row.quote
+        .col-md-8
+          blockquote
+            p
+              | Canada is a leader in integrating geospatial data and making it publicly available for decision makers. This renewed Accord will continue to ensure we have the highest level of intergovernmental collaboration to maximize efficiency while bringing benefits to nearly every sector of the economy.
+            |  
+            strong Greg Rickford
+            |  
+            em
+              | Canada&rsquo;s Minister of Natural Resources and Minister for the Federal Economic Development Initiative for Northern Ontario
+        .col-md-4
+          img.img-responsive(src='/static/img/headshot.jpg')
+    .well.bg-topography
+      .container
+        .row
+          .col-md-12
+            h2 Well with Topography background
+          .col-md-6
+            ul
+              li
+                | The use of geospatial information contributed $20.7 billion of Canada&rsquo;s national Gross Domestic Product and generated approximately 19,000 jobs within Canada&rsquo;s economy in 2013.
+              li
+                | Natural Resources Canada connects producers and users of geospatial information via the GeoConnections program, housed within the Canada Centre for Mapping and Earth Observation.
+          .col-md-6
+            ul
+              li
+                | Geomatics is the modern discipline of gathering, storing, processing, and delivering geographic information.
+              li
+                | Geographic Information System technology, or GIS, was invented in Canada in the 1960s, leading to rapid innovation in the use and application of geospatial data.
+    .container
+      .row
+        .col-md-6
+          h2 Who We Are
+          p
+            | Council members come from the governments of Canada, the provinces and the territories. Council members usually represent their government&rsquo;s major geomatics organizations. The council&rsquo;s key objective is to advance geomatics activities between jurisdictions.
+        .col-md-6
+          h2 Vision
+          p
+            | Our vision is that Canadians and governments can access the best available geographic information to support informed decision making through interoperable systems and data that are maintained in an efficient and effective manner by all Canadian jurisdictions.
+    .banner(style='background-image:url(/static/img/banner_02.jpg)')
+    .container
+      .row
+        .col-md-6
+          h2 Who We Are
+          p
+            | Council members come from the governments of Canada, the provinces and the territories. Council members usually represent their government&rsquo;s major geomatics organizations. The council&rsquo;s key objective is to advance geomatics activities between jurisdictions.
+        .col-md-6
+          h2 Vision
+          p
+            | Our vision is that Canadians and governments can access the best available geographic information to support informed decision making through interoperable systems and data that are maintained in an efficient and effective manner by all Canadian jurisdictions.
       .welcome
         .mask.top
         .mask.bottom
@@ -50,7 +139,7 @@
       .container
         .row
           .col-md-4
-            img(src='/static/img/bg4.png')
+            img(src='/static/img/montreal_point_cloud.png')
             h3 Geomatics Accord
             p 
               a.btn.btn-primary(href='#/accord') read about the Canadian Geomatics Accord
@@ -61,14 +150,10 @@
             p 
               a.btn.btn-primary(href='#/publications') publications from the council			
           .col-md-4
-            img(src='/static/img/bg5.png')
+            img(src='/static/img/radarsat_vancouver.jpg')
             h3 Issues of Interest
             p
               a.btn.btn-primary(href='#/issues') explore issues of interest 
-              
-		
-         
-          
     CCOGFooter
 </template>
 
@@ -114,25 +199,6 @@ export default {
     top 521px
     background white
     clip-path inset(0% 0% 50% 0%)
-
-  .welcome
-    text-align center
-    padding-top 40px
-    width 100%
-    height 650px
-    background-image url('/static/img/bg2.jpg')
-    background-size 100% 650px
-    h1
-      font-size 48px
-    .well
-      h2
-        font-size 36px
-        line-height 45px
-      padding 20px
-      margin auto
-      height 300px
-      width 600px
-      background-color rgba(255,255,255,0.7)
 
     .didyouknow
       h3
