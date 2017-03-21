c88afa9480a8ec7d106cfb0673cc729fbe247b67
Jason Suwala <jsuwala@hatfieldgroup.com>
2017-03-20 18:43:08 -0700

    Update to members.vue
    Added provincial and territorial members, and links to their data portals.

--- src/components/Members.vue
+++ src/components/Members.vue
@@ -4,26 +4,86 @@
     main.container(role='main', property='mainContentOfPage')
       h1 Member Organizations
 
+      img#canada.hidden(src='/static/img/canada.jpg')
+
+      
+      p Canadian Council on Geomatics members come from the Provinces, Territories and Federal Government Departments:
+      
+      h2 Provincial and Territorial Members
+      p.small Expand to find links to each member's geospatial or open data portal.
       .row
         .col-md-6
-          ul
-            li Agriculture and Agrifood Canada
-            li Canada Space Agency
-            li Fisheries and Ocean
-            li National Defence
-            li Elections Canada
-            li Environment Canada
-            li Polar Knowledge Canada
-            li Public Safety Canada
-            li Statistics Canada
-            li Public Works and Government Services Canada
-        .col-md-6
-          img#canada(src='/static/img/canada.jpg')
-          .caption
-            | Mosaic of Canada
+          details
+            summary Alberta	
+            a(href='http://open.alberta.ca/' target='_blank') Alberta Open Government Portal
+                   
+          details
+            summary British Columbia	
+            a(href='https://data.gov.bc.ca/' target='_blank') DataBC
+             
+          details
+            summary Manitoba	            
+            a(href='http://mli2.gov.mb.ca/' target='_blank') Manitoba Land Initiative
+
+          details
+            summary New Brunsick	
+            a(href='http://www.snb.ca/geonb1/' target='_blank') GeoNB
+
+          details
+            summary Newfoundland and Labrador	
+            a(href='http://opendata.gov.nl.ca/' target='_blank') Newfoundland and Labrador Open Data
+            
+          details
+            summary North West Territories	
+            a(href='http://www.geomatics.gov.nt.ca/' target='_blank') Northwest Territories Centre for Geomatics
             br
-            | &copy; 
-            a(href='http://www.asc-csa.gc.ca/eng/search/images/watch.asp?id=2603&search=radar') Canadian Space Agency
+            a(href='http://nwtdiscoveryportal.enr.gov.nt.ca/' target='_blank') NWT Discovery Portal
+
+          details
+            summary Nova Scotia		
+            a(href='https://data.novascotia.ca/' target='_blank') Nova Scotia Open Data Portal
+            br
+            a(href='http://gov.ns.ca/geonova/home/default.asp' target='_blank') GeoNova
+        .col-md-6
+          details
+            summary Nunavut
+            a(href='http://cngo.ca/' target='_blank') Canada-Nunavut Geoscience Office
+          
+          details
+            summary Ontario
+            a(href='http://www.ontario.ca/government/government-ontario-open-data' target='_blank') Ontario open data website
+
+          details
+            summary Prince Edward Island	
+            a(href='http://www.gov.pe.ca/gis/' target='_blank') GIS Data Layers: Geographic Information for PEI
+          
+          details
+            summary Quebec	
+            a(href='http://www.quebecgeographique.gouv.qc.ca/' target='_blank') Québec géographique
+            br
+            a(href='https://www.donneesquebec.ca/fr/' target='_blank') Données ouvertes
+          
+          details
+            summary Saskatchewan
+            a(href='http://gis.saskatchewan.ca') GIS.saskatchewan.ca
+          
+          details
+            summary Yukon
+            a(href='http://www.geomaticsyukon.ca/') Geomatics Yukon
+            
+      
+      h2 Federal Government Members:
+      ul
+        li Agriculture and Agrifood Canada
+        li Canada Space Agency
+        li Elections Canada
+        li Environment Canada        
+        li Fisheries and Ocean
+        li National Defence
+        li Polar Knowledge Canada
+        li Public Works and Government Services Canada        
+        li Public Safety Canada
+        li Statistics Canada
 
     CCOGFooter
 </template>
