28ef977f72f61ec9b24584b3025667e14114360c
Adam Soltys <asoltys@gmail.com>
2017-03-22 10:49:04 -0700

    toggle mobile menu

--- src/components/CCOGHeader.vue
+++ src/components/CCOGHeader.vue
@@ -8,19 +8,40 @@
               img(src='/static/img/ccog-ccdg.png', alt='Canadian Council on Geomatics - Conseil canadien de geomatique')
           .col-xs-4
             #wb-glb-mn.wb-mb-links.visible-sm.visible-xs
-              h2 Search and menus
+              h2 Menu
               ul.pnl-btn.list-inline.text-right
                 li
-                  a.overlay-lnk.btn.btn-sm.btn-default(href='#mb-pnl', title='Search and menus', aria-controls='mb-pnl', role='button')
+                  a.overlay-lnk.btn.btn-sm.btn-default(v-on:click='show = !show', href='#mb-pnl', title='Search and menus', aria-controls='mb-pnl', role='button')
                     span.glyphicon.glyphicon-th-list
-                      span.wb-inv Search and menus
+                      span.wb-inv Menu
               #mb-pnl
-          .col-md-6
+          .col-md-6.visible-md.visible-lg
             ul.list-inline
               li
                 a(lang='fr', href='https://fileexchange.nrcan.gc.ca') Sign In
               li
                 router-link(to='/fr/bienvenue') Fran&ccedil;ais
+
+
+    nav(v-if='show', role='navigation')
+      .container
+        ul.list-inline.mobile
+          li
+            router-link(to='/fr/bienvenue') Fran&ccedil;ais
+          li
+            router-link(to='welcome') {{Welcome}}
+          li
+            router-link(to='about') What We Do
+          li
+            router-link(to='issues') Issues of Interest
+          li
+            router-link(to='members') Member Organizations
+          li
+            router-link(to='accord') Geomatics Accord
+          li
+            router-link(to='publications') Publications
+          li
+            a(lang='fr', href='https://fileexchange.nrcan.gc.ca') Sign In
 
     nav.visible-md.visible-lg(role='navigation')
       .container
@@ -46,7 +67,8 @@ export default {
   name: 'CCOGHeader',
   data () {
     return {
-      Welcome: 'Welcome'
+      Welcome: 'Welcome',
+      show: false
     }
   }
 }
@@ -100,39 +122,38 @@ export default {
     list-style none
     margin-left -5px
 
-  @media screen and (max-width: 767px), screen and (max-device-width: 767px)
-    .menu
-      li
-        display block
-        a
-          text-align center
-          text-decoration none
-          color $color-primary
-          display block
-          font-weight 700
-          padding 1em
-          &:hover
-            color $color-secondary
-
-  @media screen and (min-width: 768px) and (min-device-width: 768px)
-    .menu
-      display table
-      margin 0
+  .mobile
+    li
+      display block
       width 100%
-      margin-left 0
-      position relative
-      height 56px
-      li
-        display table-cell
-        float none
-        padding 0
-        a
-          text-align center
-          text-decoration none
-          color $color-primary
-          display block
-          font-weight 700
-          padding 1em
-          &:hover
-            color $color-secondary
+      a
+        text-decoration none
+        color $color-primary
+        display block
+        font-weight 700
+        padding 1em
+        &:hover
+          color $color-secondary
+
+  .menu
+    display table
+    margin 0
+    width 100%
+    margin-left 0
+    position relative
+    height 56px
+    li
+      display table-cell
+      float none
+      padding 0
+      a
+        text-align center
+        text-decoration none
+        color $color-primary
+        display block
+        font-weight 700
+        padding 1em
+        &:hover
+          color $color-secondary
+
 </style>
