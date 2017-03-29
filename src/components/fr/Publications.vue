64d1033eec99ba73fd7fb0740444d775efc553c2
Adam Soltys <asoltys@gmail.com>
2017-03-29 11:35:32 -0700

    translated publications

--- src/components/fr/Publications.vue
+++ src/components/fr/Publications.vue
@@ -4,7 +4,7 @@
     main.container(role='main', property='mainContentOfPage')
       h1 Publications
 
-      p The Canadian Council on Geomatics has supported the development of a number of publications on topics in the field of Geomatics.
+      p Le Conseil canadien de géomatique a soutenu le développement d'un certain nombre de publications sur des sujets dans le domaine de la géomatique.
 
       .row
         .col-md-4
@@ -14,8 +14,8 @@
             br
             | &copy; USGS
         .col-md-8
-          h2 Canadian Geomatics Environmental Scan and Value Study
-          p The Canadian Geomatics Environmental Scan and Value Study report is meant to provide a new perspective on the value of geospatial information in Canada, including the state of the geomatics sector in Canada, global trends involving geospatial information and Canada’s position relative to those trends, the significance and value of the geomatics sector and of geospatial information to the Canadian economy, and current and emerging roles of government, industry and academia in supporting and using geospatial information.
+          h2 Analyse de la conjoncture du secteur canadien de la géomatique et étude sur la valeur
+          p Le pr&eacute;sent rapport regroupe les r&eacute;sultats d&eacute;coulant de tous les secteurs d'examen pour cette partie de l'Analyse de la conjoncture du secteur canadien de la g&eacute;omatique et &eacute;tude sur la valeur. L'analyse a &eacute;t&eacute; motiv&eacute;e par les pr&eacute;occupations croissantes concernant le manque de compr&eacute;hension du r&ocirc;le du secteur de la g&eacute;omatique, ainsi que de sa contribution &agrave; l'&eacute;conomie et &agrave; la soci&eacute;t&eacute; canadiennes, et de l'avenir du secteur dans une p&eacute;riode de transformation rapide du march&eacute; des produits et services d'information g&eacute;ospatiale(IG). Avec l'encouragement de la Table ronde de la communaut&eacute; canadienne de g&eacute;omatique, Ressources naturelles Canada a entrepris l'analyse, en partie pour appuyer l'&eacute;laboration d'une strat&eacute;gie pancanadienne de g&eacute;omatique.Cette partie de l'analyse a trac&eacute; le profil actuel du secteur de la g&eacute;omatique au Canada, y compris les aspects li&eacute;s &agrave; l'industrie, au gouvernement et au milieu universitaire, ainsi que les d&eacute;fis &agrave; relever et les possibilit&eacute;s &agrave; saisir par chacun d'eux, a examin&eacute; les march&eacute;s nationaux et internationaux pour les produits et les services d'IG, ainsi que les tendances et les facteurs de croissance dans le march&eacute;, et a explor&eacute; les caract&eacute;ristiques de l'offre et de la demande du march&eacute; du travail.
           a.btn.btn-primary(href='http://geoscan.nrcan.gc.ca/starweb/geoscan/servlet.starweb?path=geoscan/downloade.web&search1=R=296426') View on GEOSCAN
 
       .row
@@ -27,9 +27,9 @@
             | &copy; 
             a(href='http://www.esa.int/spaceinimages/Images/2007/11/Envisat_captures_the_Canadian_province_of_Nova_Scotia') ESA
         .col-md-8
-          h2 Environmental scan on the operational use of Remotely Piloted Aircraft Systems (RPAS) for geomatics applications in Canada
-          p Remotely-piloted Aircraft Systems, or drones, have emerged as an excellent tool to monitor small sites of interest. They play an increasingly important role within organizations by providing a less costly, higher detailed, and more flexible method to collect information compared to conventional methods. This report helps organizations determine whether drones are a suitable alternative and how to implement drones within their operations. The report provides an overview of proven applications and highlights the ready-to-fly equipment that is currently available in North America. 
-          p Organizations must address regulations concerning aviation, privacy, intellectual property, trespassing, and the transportation of dangerous goods. Best practices are summarized in terms of how to design a stand-alone project or implement an operational program, and techniques used to obtain, process, and manage 2D, 3D, and video datasets.  Risk management strategies are formulated. Emerging opportunities, recommendations, and knowledge gaps are highlighted to advance the use of drones in Canada.
+          h2 Analyse du contexte de l'utilisation des syst&egrave;mes d'a&eacute;ronefs t&eacute;l&eacute;pilot&eacute;s (drones) en g&eacute;omatique au Canada
+          p Les organisations adoptent de plus en plus les syst&egrave;mes d'a&eacute;ronefs t&eacute;l&eacute;pilot&eacute;s, ou « drones », pour la surveillance de petits sites d'int&eacute;r&ecirc;ts, car ils constituent une m&eacute;thode moins co&ucirc;teuse, plus pr&eacute;cise et plus souple de collecte d'information par rapport aux m&eacute;thodes traditionnelles. Le pr&eacute;sent rapport a &eacute;t&eacute; r&eacute;dig&eacute; pour aider les organisations &agrave; d&eacute;terminer si les drones constituent une possibilit&eacute; ad&eacute;quate et comment les int&eacute;grer &agrave; leurs activit&eacute;s. Le pr&eacute;sent rapport contient un aper&ccedil;u des utilisations &eacute;prouv&eacute;es et pr&eacute;sente les points saillants des appareils pr&ecirc;ts &agrave; voler, actuellement disponibles en Am&eacute;rique-du-Nord.
+          p Les organisations doivent aborder les r&egrave;glements relatifs &agrave; l'aviation, &agrave; la protection de la vie priv&eacute;e, &agrave; la propri&eacute;t&eacute; intellectuelle, aux intrusions et au transport de marchandises dangereuses. Le document r&eacute;sume les pratiques exemplaires de conception de projets autonomes ou de mise en place d'un programme op&eacute;rationnel ainsi que des techniques de cueillette, traitement et gestion des bases de donn&eacute;es bidimensionnelles, tridimensionnelles et vid&eacute;o. Des strat&eacute;gies de gestion des risques sont formul&eacute;es par la synth&egrave;se des r&egrave;glements et des pratiques exemplaires. Afin d'accro&icirc;tre l'utilisation de drones au Canada, nous indiquons les nouvelles possibilit&eacute;s, les recommandations et les trous dans les connaissances.
           a.btn.btn-primary(href='http://geoscan.nrcan.gc.ca/starweb/geoscan/servlet.starweb?path=geoscan/fulle.web&search1=R=299120') View on GEOSCAN
 
       .row
@@ -41,8 +41,8 @@
             | &copy; 
             a(href='https://earthobservatory.nasa.gov/IOTD/view.php?id=78527') NASA
         .col-md-8
-          h2 Federal Floodplain Mapping Framework
-          p The purpose of this document is to introduce the Federal Floodplain Mapping Guidelines Series documents and to provide a framework for how each fits into the overall floodplain mapping life cycle. Specifically, this document will provide a brief history of past and present floodplain mapping efforts in Canada, a brief summary of floodplain mapping roles and responsibilities and an overview of the contents of the suite of documents that make up the Federal Floodplain Mapping Guidelines Series.
+          h2 Cadre f&eacute;d&eacute;ral de la cartographie des plaines inondables
+          p Le gouvernement du Canada a &eacute;labor&eacute; le document intitul&eacute; « Cadre f&eacute;d&eacute;ral de la cartographie des plaines inondables &agrave; l'appui des efforts d'att&eacute;nuation des inondations ». Ce document a &eacute;t&eacute; con&ccedil;u en vue de pr&eacute;senter la s&eacute;rie « Guides d'orientation f&eacute;d&eacute;raux sur la cartographie des plaines inondables » et de fournir un r&eacute;sum&eacute; du contenu de chaque document de la s&eacute;rie et un cadre sur la fa&ccedil;on dont ces documents s'harmonisent.
           a.btn.btn-primary(href='http://geoscan.nrcan.gc.ca/starweb/geoscan/servlet.starweb?path=geoscan/shorte.web&search1=R=299806') View on GEOSCAN
 
       .row
