b9e4527db497e264df8e0c5a164778b8249bd4e1
Adam Soltys <asoltys@gmail.com>
2017-03-31 11:16:15 -0700

    update french publication links

--- src/components/fr/Publications.vue
+++ src/components/fr/Publications.vue
@@ -8,21 +8,21 @@
 
       .row
         .col-md-4
-          img(src='/static/img/mistastin_lake.jpg', alt='Mistastin Lake')
+          img(src='/static/img/mistastin_lake.jpg', alt='Lac Mistastin')
           .caption
-            | Mistastin Lake
+            | Lac Mistastin
             br
             | &copy; USGS
         .col-md-8
-          h2 Analyse de la conjoncture du secteur canadien de la géomatique et étude sur la valeur
+          h2 Rapport des résultats de l’analyse de la conjoncture du secteur canadien de la géomatique
           p Le pr&eacute;sent rapport regroupe les r&eacute;sultats d&eacute;coulant de tous les secteurs d'examen pour cette partie de l'Analyse de la conjoncture du secteur canadien de la g&eacute;omatique et &eacute;tude sur la valeur. L'analyse a &eacute;t&eacute; motiv&eacute;e par les pr&eacute;occupations croissantes concernant le manque de compr&eacute;hension du r&ocirc;le du secteur de la g&eacute;omatique, ainsi que de sa contribution &agrave; l'&eacute;conomie et &agrave; la soci&eacute;t&eacute; canadiennes, et de l'avenir du secteur dans une p&eacute;riode de transformation rapide du march&eacute; des produits et services d'information g&eacute;ospatiale(IG). Avec l'encouragement de la Table ronde de la communaut&eacute; canadienne de g&eacute;omatique, Ressources naturelles Canada a entrepris l'analyse, en partie pour appuyer l'&eacute;laboration d'une strat&eacute;gie pancanadienne de g&eacute;omatique.Cette partie de l'analyse a trac&eacute; le profil actuel du secteur de la g&eacute;omatique au Canada, y compris les aspects li&eacute;s &agrave; l'industrie, au gouvernement et au milieu universitaire, ainsi que les d&eacute;fis &agrave; relever et les possibilit&eacute;s &agrave; saisir par chacun d'eux, a examin&eacute; les march&eacute;s nationaux et internationaux pour les produits et les services d'IG, ainsi que les tendances et les facteurs de croissance dans le march&eacute;, et a explor&eacute; les caract&eacute;ristiques de l'offre et de la demande du march&eacute; du travail.
-          a.btn.btn-primary(href='http://geoscan.nrcan.gc.ca/starweb/geoscan/servlet.starweb?path=geoscan/downloade.web&search1=R=296426') View on GEOSCAN
+          a.btn.btn-primary(href='http://geoscan.nrcan.gc.ca/starweb/geoscan/servlet.starweb?path=geoscan/shorte.web&search1=R=299806') Vue sur GEOSCAN
 
       .row
         .col-md-4
-          img(src='/static/img/nova_scotia.jpg', alt='Envisat captures the Canadian province of Nova Scotia')
+          img(src='/static/img/nova_scotia.jpg', alt='Envisat capture la province canadienne de Nouvelle-Écosse')
           .caption
-            | Envisat captures the Canadian province of Nova Scotia
+            | Envisat capture la province canadienne de Nouvelle-Écosse
             br
             | &copy; 
             a(href='http://www.esa.int/spaceinimages/Images/2007/11/Envisat_captures_the_Canadian_province_of_Nova_Scotia') ESA
@@ -30,29 +30,25 @@
           h2 Analyse du contexte de l'utilisation des syst&egrave;mes d'a&eacute;ronefs t&eacute;l&eacute;pilot&eacute;s (drones) en g&eacute;omatique au Canada
           p Les organisations adoptent de plus en plus les syst&egrave;mes d'a&eacute;ronefs t&eacute;l&eacute;pilot&eacute;s, ou « drones », pour la surveillance de petits sites d'int&eacute;r&ecirc;ts, car ils constituent une m&eacute;thode moins co&ucirc;teuse, plus pr&eacute;cise et plus souple de collecte d'information par rapport aux m&eacute;thodes traditionnelles. Le pr&eacute;sent rapport a &eacute;t&eacute; r&eacute;dig&eacute; pour aider les organisations &agrave; d&eacute;terminer si les drones constituent une possibilit&eacute; ad&eacute;quate et comment les int&eacute;grer &agrave; leurs activit&eacute;s. Le pr&eacute;sent rapport contient un aper&ccedil;u des utilisations &eacute;prouv&eacute;es et pr&eacute;sente les points saillants des appareils pr&ecirc;ts &agrave; voler, actuellement disponibles en Am&eacute;rique-du-Nord.
           p Les organisations doivent aborder les r&egrave;glements relatifs &agrave; l'aviation, &agrave; la protection de la vie priv&eacute;e, &agrave; la propri&eacute;t&eacute; intellectuelle, aux intrusions et au transport de marchandises dangereuses. Le document r&eacute;sume les pratiques exemplaires de conception de projets autonomes ou de mise en place d'un programme op&eacute;rationnel ainsi que des techniques de cueillette, traitement et gestion des bases de donn&eacute;es bidimensionnelles, tridimensionnelles et vid&eacute;o. Des strat&eacute;gies de gestion des risques sont formul&eacute;es par la synth&egrave;se des r&egrave;glements et des pratiques exemplaires. Afin d'accro&icirc;tre l'utilisation de drones au Canada, nous indiquons les nouvelles possibilit&eacute;s, les recommandations et les trous dans les connaissances.
-          a.btn.btn-primary(href='http://geoscan.nrcan.gc.ca/starweb/geoscan/servlet.starweb?path=geoscan/fulle.web&search1=R=299120') View on GEOSCAN
+          a.btn.btn-primary(href='http://geoscan.nrcan.gc.ca/starweb/geoscan/servlet.starweb?path=geoscan/fullf.web&search1=R=299120') Vue sur GEOSCAN
 
       .row
         .col-md-4
-          img(src='/static/img/nunavut.jpg', alt='Canadian Land of Lakes')
+          img(src='/static/img/nunavut.jpg', alt='Terre canadienne des lacs')
           .caption
-            | Canadian Land of Lakes
+            | Terre canadienne des lacs
             br
             | &copy; 
             a(href='https://earthobservatory.nasa.gov/IOTD/view.php?id=78527') NASA
         .col-md-8
           h2 Cadre f&eacute;d&eacute;ral de la cartographie des plaines inondables
           p Le gouvernement du Canada a &eacute;labor&eacute; le document intitul&eacute; « Cadre f&eacute;d&eacute;ral de la cartographie des plaines inondables &agrave; l'appui des efforts d'att&eacute;nuation des inondations ». Ce document a &eacute;t&eacute; con&ccedil;u en vue de pr&eacute;senter la s&eacute;rie « Guides d'orientation f&eacute;d&eacute;raux sur la cartographie des plaines inondables » et de fournir un r&eacute;sum&eacute; du contenu de chaque document de la s&eacute;rie et un cadre sur la fa&ccedil;on dont ces documents s'harmonisent.
-          a.btn.btn-primary(href='http://geoscan.nrcan.gc.ca/starweb/geoscan/servlet.starweb?path=geoscan/shorte.web&search1=R=299806') View on GEOSCAN
+          a.btn.btn-primary(href='http://geoscan.nrcan.gc.ca/starweb/geoscan/servlet.starweb?path=geoscan/fullf.web&search1=R=299807') Vue sur GEOSCAN
 
       .row
         .col-md-12
-          h2 Other Publications
-          p 
-            | The 
-            a(href='https://www.nrcan.gc.ca/earth-sciences/geomatics/canadas-spatial-data-infrastructure/8904') CGDI Resource Centre 
-            | summarizes potentially relevant publications that are available on GEOSCAN.
-          a 
+          h2 Autres publications
+          p Le #[a(href='https://www.rncan.gc.ca/sciences-terre/geomatique/infrastructure-canadienne-donnees-spatiales/8905') Centre de ressources de l'ICDG] résume les publications potentiellement pertinentes disponibles sur GEOSCAN.
     CCOGFooter
 </template>
 
